30c2528e829f973934d7ff5f44550e1ee9bbd023
Prutagat <prutagat@yandex.ru>
2023-05-28 20:57:01 +0300

    Домашнее задание 2.3 часть 2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -7,20 +7,13 @@ class ProfileViewController: UIViewController {
     
     private let profileHeaderView = ProfileHeaderView()
     
-    private lazy var newButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Новая кнопка", for: .normal)
-        button.setTitleColor(.white, for: .normal)
-        button.backgroundColor = .systemBlue
-        button.clipsToBounds = true
-        button.layer.cornerRadius = 8
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.layer.shadowOffset = CGSize(width: 4, height: 4)
-        button.layer.shadowRadius = 4.0
-        button.layer.shadowOpacity = 0.7
-        
-        return button
+    private let tableView: UITableView =  {
+        let tableView = UITableView.init()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
     }()
+    
+    private var dataSource = PostModel.makeDataSource()
     
     // MARK: - Lifecycle
     
@@ -29,9 +22,10 @@ class ProfileViewController: UIViewController {
         
         setupView()
         addSubviews()
+        setupTable()
         setupConstraints()
     }
-
+    
     // MARK: - Private
     
     private func setupView() {
@@ -41,20 +35,46 @@ class ProfileViewController: UIViewController {
     
     private func addSubviews() {
         view.addSubview(profileHeaderView)
-        view.addSubview(newButton)
+        view.addSubview(tableView)
+        
+    }
+    
+    private func setupTable() {
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.id)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     private func setupConstraints() {
+        let safeAreaGuide = view.safeAreaLayoutGuide
+        
         NSLayoutConstraint.activate([
-            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            profileHeaderView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            profileHeaderView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: safeAreaGuide.topAnchor),
+            profileHeaderView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
             profileHeaderView.heightAnchor.constraint(equalToConstant:220),
             
-            newButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            newButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            tableView.topAnchor.constraint(equalTo: profileHeaderView.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaGuide.bottomAnchor)
         ])
+    }
+    
+}
+
+extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dataSource.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.id, for: indexPath) as? PostTableViewCell
+        else { return UITableViewCell() }
+        let post = dataSource[indexPath.row]
+        cell.configure(with: post)
+        return cell
     }
     
 }
