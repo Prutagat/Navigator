233d57e5c58556a3be931511dac97971bd303fa5
Prutagat <a.v.golovanov@icloud.com>
2023-09-28 17:42:42 +0300

    Домашнее задание 1.2
    Домашнее задание к занятию 1.2. «JSON. Codable. Модели данных»

--- Navigation/Modules/FeedFlow/InfoViewController.swift
+++ Navigation/Modules/FeedFlow/InfoViewController.swift
@@ -5,10 +5,32 @@ import SnapKit
 class InfoViewController: UIViewController {
 
     let coordinator: FeedCoordinator
+    var residents: [Resident] = []
     
-    private lazy var actionButton = CustomButton(title: "Открыть", cornerRadius: 10) { [weak self] in
+    private lazy var actionButton = CustomButton(title: "Сообщить", cornerRadius: 10) { [weak self] in
         self?.coordinator.present(.attention("Кря кря кря"))
     }
+    private let taskFirst: UILabel = {
+        let lable = UILabel()
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.clipsToBounds = true
+        lable.numberOfLines = 3
+        return lable
+    }()
+    private let taskSecond: UILabel = {
+        let lable = UILabel()
+        lable.translatesAutoresizingMaskIntoConstraints = false
+        lable.clipsToBounds = true
+        lable.numberOfLines = 3
+        return lable
+    }()
+    private lazy var taskThird: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
     
     init(coordinator: FeedCoordinator) {
         self.coordinator = coordinator
@@ -21,14 +43,86 @@ class InfoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemMint
+        getTitle()
+        getOrbitalPeriod()
+        getResidents()
+        view.backgroundColor = .systemGray4
         view.addSubview(actionButton)
+        view.addSubview(taskFirst)
+        view.addSubview(taskSecond)
+        view.addSubview(taskThird)
         
         actionButton.snp.makeConstraints { make in
-            make.centerX.centerY.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.bottom.equalToSuperview().offset(-130)
             make.leading.equalTo(20)
             make.trailing.equalTo(-20)
             make.height.equalTo(44)
         }
+        taskFirst.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(actionButton.snp.top).offset(-16)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(44)
+        }
+        taskSecond.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(taskFirst.snp.top).offset(-16)
+            make.leading.equalTo(20)
+            make.trailing.equalTo(-20)
+            make.height.equalTo(44)
+        }
+        taskThird.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.bottom.equalTo(taskSecond.snp.top).offset(-16)
+        }
+    }
+    
+    func getTitle() {
+        NetworkService.requestUser(id: Int.random(in: 1..<51)) { [weak self] name in
+            self?.taskFirst.text = "Задание 1: \(name)"
+        }
+    }
+    
+    func getOrbitalPeriod() {
+        NetworkService.requestPlanet(id: Int.random(in: 1..<6)) { [weak self] planet in
+            self?.taskSecond.text = "Задание 2: Период вращения планеты \(planet.name) вокруг своей звезды - \(planet.rotationPeriod)"
+        }
+    }
+    
+    func getResidents() {
+        NetworkService.requestPlanet(id: 1) { [weak self] planet in
+            for resident in planet.residents {
+                NetworkService.requestResident(url: resident) { residentName in
+                    self?.residents.append(Resident(name: residentName, URL: resident))
+                    self?.taskThird.reloadData()
+                }
+            }
+        }
+    }
+}
+
+extension InfoViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        residents.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var content: UIListContentConfiguration = cell.defaultContentConfiguration()
+        content.text = residents[indexPath.row].name
+        content.secondaryText = residents[indexPath.row].URL
+        cell.contentConfiguration = content
+        return cell
+    }
+}
+
+extension InfoViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        let controller = YoutubeVideoView(url: people[indexPath.row].URL)
+//        present(controller, animated: true)
     }
 }
